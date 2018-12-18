97bf75f0e87fef38919f2b693bd45af37c0821dc
juliannapaylor <julianna.p756@slcstudents.org>
2018-12-18 08:23:33 -0700

    added variable stuff

--- myScript.js
+++ myScript.js
@@ -1,3 +1,22 @@
+//This function changed the text in the demo paragraph 
+
 function myFunction () {
     document.getElementById('demo').innerHTML = "Paragraph Changed";
+}
+
+//Defining variables
+
+//var x = 5;
+//var y = 6;
+//var z = x + y;
+
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+//This function changes the text in teh math paragraph
+
+function mathFunction () {
+    document.getElementById('math').innerHTML = z;
 }
