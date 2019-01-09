b2ab45797068213f7d4591219f61b752f52f6991
juliannapaylor <julianna.p756@slcstudents.org>
2019-01-09 08:50:25 -0700

    variables

--- myScript.js
+++ myScript.js
@@ -4,19 +4,6 @@ function myFunction () {
     document.getElementById('demo').innerHTML = "Paragraph Changed";
 }
 
-//Defining variables
-
-//var x = 5;
-//var y = 6;
-//var z = x + y;
-
-var x, y, z;
-x = 5;
-y = 6;
-z = x + y;
-
-//This function changes the text in teh math paragraph
-
-function mathFunction () {
-    document.getElementById('math').innerHTML = z;
+function math(p1, p2) {
+    return p1 + p2;
 }
